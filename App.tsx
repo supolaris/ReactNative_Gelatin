f6a50b767a33c1620aaafb42b2d31ad92af10e01
Muhammad Suleman <msulaman061@gmail.com>
2023-10-27 17:15:43 +0500

    stack navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
 import {View, Text, StyleSheet} from "react-native";
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
 
 const App = () => {
+
+
+
+  const Stack = createStackNavigator();
+  function ScreenA() {
+    return(
+      <View>
+        <Text>Screen A</Text>
+      </View>
+    )
+    
+  }
+
+  function ScreenB() {
+    return(
+      <View>
+        <Text>Screen B</Text>
+      </View>
+    )
+    
+  }
+
+
   return(
-    <View style={myStyle.container}>
-      <Text style ={myStyle.text}>Hello world</Text>
-    </View>
+    <NavigationContainer>
+
+      <Stack.Navigator>
+        <Stack.Screen
+
+        name="First Screen"
+        component={ScreenA}
+        />
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
   )
 }
 
